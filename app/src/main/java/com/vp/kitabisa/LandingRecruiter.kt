6d6006c79636c6dcd60fe1bc22de8fbbf044594e
Jovan Louis Lemuel <jovanlemuel9103@gmail.com>
2022-12-12 01:32:17 +0700

    Initial commit

---
+++ app/src/main/java/com/vp/kitabisa/LandingRecruiter.kt
@@ -0,0 +1,11 @@
+package com.vp.kitabisa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LandingRecruiter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing_recruiter)
+    }
+}
